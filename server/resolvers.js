419d12a93328d8f07bfe7ec910e308e3a0f028cd
Italo Nabuco <italonabuco@hotmail.com>
2023-05-29 12:38:31 -0300

    create type Job and non-nullability

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,5 +1,18 @@
 export const resolvers = {
   Query: {
-    greeting: () => 'Hello, Italo',
+    jobs: () => {
+      return [
+        {
+          id: 'job-id-1',
+          title: 'The title 1',
+          description: 'The description',
+        },
+        {
+          id: 'job-id-2',
+          title: 'The title 2',
+          description: 'The description',
+        },
+      ];
+    },
   },
 };
