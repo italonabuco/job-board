5196986ad657c9246875258d803942de62abe8fc
Italo Nabuco <italonabuco@hotmail.com>
2023-05-29 12:16:02 -0300

    set up apollo server and graphql

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 import cors from 'cors';
 import express from 'express';
 import { authMiddleware, handleLogin } from './auth.js';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware as apolloMiddleware } from '@apollo/server/express4';
+import { readFile } from 'node:fs/promises';
+import { resolvers } from './resolvers.js';
 
 const PORT = 4000;
 
@@ -9,6 +13,17 @@ app.use(cors(), express.json(), authMiddleware);
 
 app.post('/login', handleLogin);
 
+const typeDefs = await readFile('./schema.graphql', 'utf8'); // read and return single string
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+await apolloServer.start();
+
+// everything regarding apollo will go through this path
+app.use('/graphql', apolloMiddleware(apolloServer));
+
 app.listen({ port: PORT }, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log(`GraphQL endpoint: http://localhost:${PORT}/graphql`);
 });
