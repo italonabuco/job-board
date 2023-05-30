10ac6b3ea2402f0e7f308403206bda68fd3b7f91
Italo Nabuco <italonabuco@hotmail.com>
2023-05-30 13:42:32 -0300

    fix createCompanyLoader

--- server/db/companies.js
+++ server/db/companies.js
@@ -7,11 +7,11 @@ export async function getCompany(id) {
   return await getCompanyTable().first().where({ id });
 }
 
-export function createCompanyLoader(){
+export function createCompanyLoader() {
   return new DataLoader(async (ids) => {
     console.log('[companyLoader] ids', ids);
     const companies = await getCompanyTable().select().whereIn('id', ids);
     // data loader requires that data should be return in the same order as the keys we are requesting
-    return ids.map((id) => companies.find((company) => (company.id = id)));
+    return ids.map((id) => companies.find((company) => company.id === id));
   });
 }
